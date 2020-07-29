6a74ca79eaa68bec81e82f425f5029d5f01740b4
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2020-07-29 10:31:07 -0500

    cosmetics

--- examples/example_truncate_launcher.py
+++ examples/example_truncate_launcher.py
@@ -6,6 +6,5 @@ import pylauncher3
 ## Classic launcher with a per-task timeout
 ##
 
-#pylauncher.ClassicLauncher("corecommandlines",debug="job+host+task")
 pylauncher3.ClassicLauncher("commandlines",taskmaxruntime=30,delay=1,debug="job+host")
 
