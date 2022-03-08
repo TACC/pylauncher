43027f7da239b627a4190ea21734a596d666b2f7
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2022-03-08 10:04:38 -0600

    clarify paths

--- examples/local_paths.sh
+++ examples/local_paths.sh
@@ -1,8 +1,9 @@
 ################################################################
 ####
 #### Set up python path for local testing in the repository
-#### THIS NEEDS TO BE SOURCED, NOT EXECUTED
+#### THIS NEEDS TO BE SOURCED, NOT EXECUTED,
+#### and in this location!
 ####
 ################################################################
 
-export PYTHONPATH=..:${PYTHONPATH}
+export PYTHONPATH=`pwd`/..:${PYTHONPATH}
