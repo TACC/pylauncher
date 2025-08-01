4c89c4ce6ddb6cc866da9dda9b6fee35755e1540
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-08-01 12:18:12 -0500

    project parameter for test script

---
+++ tests/example_manylines_launcher.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+################################################################
+####
+#### This file is part of the `pylauncher' package
+#### for parametric job launching
+####
+#### Copyright Victor Eijkhout 2010-2025
+#### eijkhout@tacc.utexas.edu
+####
+#### https://github.com/TACC/pylauncher
+####
+################################################################
+
+import os
+import pylauncher
+
+##
+## Emulate the classic launcher, using a one liner
+##
+
+example = "ClassicLauncher"
+pylauncher.ClassicLauncher\
+    ("manycommandlines",
+     # optional spec of output dir:
+     workdir=f"pylauncher_tmp_{example}_{ os.environ['SLURM_JOBID'] }",
+     debug="host+exec+task+job")
